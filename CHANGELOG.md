c57944d43e344c2cf5f1cb5f74ca320650553c9c
Kioshi Okamoto Rojas <kioshi.okamoto@trade.ec>
2022-04-17 23:36:58 -0500

    feat(firebase): add enviroments and firebase config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,6 @@ All notable changes to this project will be documented in this file. See [standa
 
 ### 0.1.1 (2022-04-07)
 
-
 ### Features
 
-* **global:** initialize project ([efe4e7e](https://github.com/kioshiokamoto/crowdfunding-front/commit/efe4e7ead4a53acb74ccde2e868fb4bb5dd717fb))
+- **global:** initialize project ([efe4e7e](https://github.com/kioshiokamoto/crowdfunding-front/commit/efe4e7ead4a53acb74ccde2e868fb4bb5dd717fb))
---
+++ src/lib/utils/firebaseConfig.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+};
+
+let firebaseApp;
+if (firebase.apps.length) {
+  firebaseApp = firebase.app(firebase.apps[0].name);
+} else {
+  firebaseApp = firebase.initializeApp(firebaseConfig);
+}
+
+export const app = firebaseApp;
+export const auth = () => app?.auth();
